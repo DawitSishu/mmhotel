04af18038d1a8749291b60d0e6666f16d9f49912
DawitSishu <dawitalemayhu55@gmail.com>
2023-09-08 12:10:40 -0400

    pallete setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+} from "@mui/material";
+import "./App.css";
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#800080",
+    },
+    secondary: {
+      main: "#333333",
+    },
+    background: {
+      default: "#fadefa",
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 768,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
