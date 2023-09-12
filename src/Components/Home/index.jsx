cb5093e7d7f602488391dc1f64bf0c47f1f12e26
DawitSishu <dawitalemayhu55@gmail.com>
2023-09-12 10:32:08 -0400

    home page almost done

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Navbar";
 import mainImg from "../../assets/mm.jpg";
 import Footer from "../Footer";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Divider, Box, Button, Paper } from "@mui/material";
 import { FaMapMarkerAlt, FaStar } from "react-icons/fa";
+import "./style.css";
+import { IoIosArrowForward } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const index = () => {
   return (
@@ -46,7 +49,7 @@ const index = () => {
           </Typography>
         </Grid>
       </Grid>
-      <Grid pl={10} pr={10} pb={5}>
+      <Grid pl={10} pr={10} pb={3}>
         <Typography
           variant="h4"
           color="primary"
@@ -55,7 +58,7 @@ const index = () => {
         >
           WELCOME
         </Typography>
-        <Typography variant="h5" align="center" color="secondary">
+        <Typography variant="body2" align="center">
           <FaStar color="#FF00FF" /> Nestled in the heart of the city's vibrant
           pulse, MM Hotel offers a prime location just moments away from the
           bustling bus station. Immerse yourself in the dynamic energy of Dire
@@ -63,7 +66,110 @@ const index = () => {
           cultural hotspots, and hidden gems, all conveniently accessible from
           our doorstep. <FaStar color="#FF00FF" />
         </Typography>
+        <Divider
+          sx={{
+            marginTop: 2,
+            width: "30%",
+            height: "2px",
+            margin: "20px auto",
+            backgroundColor: "#FF00FF",
+          }}
+        />
       </Grid>
+      <Grid container spacing={2} pl={2} pr={2} mb={4}>
+        <Grid item xs={12} sm={4}>
+          <div className="grid-item">
+            <img src={mainImg} alt="Background" className="background-image" />
+            <div className="overlay" />
+            <div className="content">
+              <Typography variant="h5">Rooms</Typography>
+              <br />
+              <Link to="/rooms">
+                <Button
+                  variant="outlined"
+                  sx={{ color: "white", borderColor: "white" }}
+                >
+                  continue
+                  <span style={{ marginLeft: "5px" }} />
+                  <IoIosArrowForward color="white" />
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <div className="grid-item">
+            <img src={mainImg} alt="Background" className="background-image" />
+            <div className="overlay" />
+            <div className="content">
+              <Typography variant="h5">Awards</Typography>
+              <br />
+              <Link to="/honors">
+                <Button
+                  variant="outlined"
+                  sx={{ color: "white", borderColor: "white" }}
+                >
+                  continue
+                  <span style={{ marginLeft: "5px" }} />
+                  <IoIosArrowForward color="white" />
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <div className="grid-item">
+            <img src={mainImg} alt="Background" className="background-image" />
+            <div className="overlay" />
+            <div className="content">
+              <Typography variant="h5">Gallery</Typography>
+              <br />
+              <Link to="/gallery">
+                <Button
+                  variant="outlined"
+                  sx={{ color: "white", borderColor: "white" }}
+                >
+                  continue
+                  <span style={{ marginLeft: "5px" }} />
+                  <IoIosArrowForward color="white" />
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+      <Box>
+        <Grid container justifyContent="center" spacing={2} mb={3}>
+          <Grid item xs={12}>
+            <Typography variant="h3" align="center" color="primary">
+              Our Services
+            </Typography>
+          </Grid>
+          <Grid container justifyContent="center">
+            <Box
+              sx={{
+                width: "60%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="body2" align="center">
+                To meet your every need, our hotel provides tailored and unique
+                experiences, as well as individual concierge services. Our crew
+                is dedicated to assuring your entire happiness, allowing you to
+                unwind and enjoy every second of your stay. Explore our unique
+                packages for a memorable luxury vacation experience now.
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <br />
+      <br />
+      <br />
+      <div></div>
     </div>
   );
 };
